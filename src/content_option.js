f5ab61f656e311862ffdb7c5ff270725101c8bc5
Rishabh Vishwakarma <rishabhvishwakarma101@gmail.com>
2024-04-14 06:20:52 +1000

    portfolio commit

---
+++ src/content_option.js
@@ -0,0 +1,177 @@
+const logotext = "RISHABH";
+const meta = {
+    title: "Rishabh Vishwakarma",
+    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+};
+
+const introdata = {
+    title: "I’m Rishabh Vishwakarma",
+    animated: {
+        first: "I love coding",
+        second: "I Do Security",
+        third: "I'm Good at Cloud as well'",
+    },
+    description: "👋I'm Rishabh, dedicated to managing IT infrastructure and ensuring robust security measures at Trikuta Infotech. With a focus on client satisfaction, I oversee cloud management, collaborate with security experts on audits, and implement comprehensive security measures for endpoints, cloud resources, exchange servers, and more. My proactive approach to monitoring and addressing cyber threats ensures that our clients' data remains secure and their operations run smoothly. Let's partner together to fortify your IT infrastructure and achieve your business goals seamlessly!",
+    your_img_url: "https://res.cloudinary.com/dgpyxbgzs/image/upload/v1713034199/Rishabh-No_Background_ikmalc_bhmw1v.png",
+   
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "Hello! 👋 I'm Rishabh, a passionate System Engineer with nearly 2 years of experience at Trikuta Infotech. I specialize in IT management, cybersecurity, and strategic consulting, ensuring top-notch solutions for our clients. With a focus on efficiency and security, I've earned trust and satisfaction from clients across Australia. My dedication to staying updated on the latest trends ensures that I deliver exceptional service and results. Let's connect and discuss how I can help elevate your business's IT infrastructure and achieve your goals!",
+};
+const worktimeline = [{
+        jobtitle: "Trikuta Infotech Pty Ltd",
+        where: "IT Manager",
+        date: "2024",
+    },
+    {
+        jobtitle: "Trikuta Infotech Pty Ltd",
+        where: "System Engineer",
+        date: "2023",
+    },
+    {
+        jobtitle: "Ondru Org",
+        where: "IT Officer",
+        date: "2022",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "Azure",
+        value: 85,
+    },
+    {
+        name: "SIEM & SOAR",
+        value: 80,
+    },
+    {
+        name: "M365",
+        value: 100,
+    },
+    {
+        name: "Networking",
+        value: 50,
+    },
+];
+
+const services = [{
+        title: "IT Consultant",
+        description: "System Engineer with a knack for IT consulting. With 2 years at Trikuta Infotech, I specialize in optimizing systems and boosting cybersecurity. Let's chat about how I can elevate your business's IT!",
+    },
+
+    {
+        title: "Wordpress Design",
+        description: "Your go-to WordPress designer. With a keen eye for design and 2 years of experience crafting stunning websites at Trikuta Infotech, I'll bring your vision to life. Let's create something amazing together!",
+    },
+    {
+        title: "SOC Analytics",
+        description: "Your trusted SOC analytics expert. With a deep understanding of cybersecurity threats and extensive experience in analyzing security data, I excel in identifying and mitigating risks. Let's strengthen your security posture with proactive monitoring and advanced analytics!",
+    },
+    {
+        title: "IT Manager",
+        description: "your dedicated IT manager. With nearly 2 years of experience at Trikuta Infotech, I specialize in overseeing IT operations and implementing strategic solutions. Let's streamline your technology and drive your business forward!",
+    },
+    {
+        title: "React and Bootstrap Developer",
+        description: "Your React and Bootstrap developer. With a passion for crafting responsive and dynamic web applications, I specialize in building modern, user-friendly interfaces. Let's bring your project to life with the power of React and Bootstrap!",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    // {
+    //     img: "https://picsum.photos/400/800/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/600/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/300/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/700/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+
+    // {
+    //     img: "https://picsum.photos/400/600/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/300/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/550/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/700/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "rishabhvishwakarma101@gmail.com",
+    YOUR_FONE: "+91 9004999826",
+    // description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/KenRishabh",
+    facebook: "https://www.facebook.com/zenitsu2106",
+    linkedin: "https://www.linkedin.com/in/rishabh-zenitsu/",
+    twitter: "https://twitter.com/Ken_Rishabh",
+    
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
